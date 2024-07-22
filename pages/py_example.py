bb63abfd34a1879c4d310fecfe3fef4f021001ef
홍서정 <94052799+hsj517@users.noreply.github.com>
2024-07-22 21:34:08 +0900

    refactor : topic 관리 딕셔너리 생성

--- pages/py_example.py
+++ pages/py_example.py
@@ -6,11 +6,30 @@ st.set_page_config(layout="wide")
 if 'page' not in st.session_state:
     st.session_state['page'] = None
 
+topics = {
+    "파이썬 기초": {
+        "대단원 01": ["소단원01", "소단원02"],
+        "대단원 02": ["소단원01"],
+        "대단원 03": ["소단원01", "소단원02", "소단원03"]
+    },
+    "pandas 기초": {
+        "대단원 01": ["소단원01", "소단원02", "소단원03"],
+        "대단원 02": ["소단원01", "소단원02"],
+        "대단원 03": ["소단원01"],
+        "대단원 04": ["소단원01", "소단원02", "소단원03", "소단원04"],
+        "대단원 05": ["소단원01", "소단원02"]
+    },
+    "Matplotlib 기초": {
+        "대단원 01": ["소단원01", "소단원02", "소단원03"],
+        "대단원 02": ["소단원01", "소단원02"]
+    }
+}
+
 # 사이드바 옵션
 with st.sidebar:
     selected = option_menu(
         "데이터 분석 역량 강화", 
-        ["파이썬 기초", "pandas 기초", "Matplotlib 기초"], 
+        list(topics.keys()), 
         default_index=0,
         styles={
             "menu-title": {"font-size": "13px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
@@ -26,23 +45,20 @@ if selected :
 
 placeholder = st.empty()
 
-
-# 파이썬 기초 페이지
-if st.session_state['page'] == "파이썬 기초":
+if st.session_state['page']:
     with placeholder.container():
-        st.title("파이썬 기초")
+        st.title(st.session_state['page'])
         st.info('파이썬 기초 문법을 제공합니다')
         
         # 대단원
-        topics = ["대단원01", "대단원02", "대단원03", "대단원04", "대단원05", "대단원06"]
-        table = [st.columns(3)] * ((len(topics) + 2) // 3)
+        middletopics = topics[st.session_state['page']]
+        table = [st.columns(3)] * ((len(middletopics) + 2) // 3)
         
-        for i, title in enumerate(topics):
+        for i, title in enumerate(middletopics):
             with table[i // 3][i % 3]:
                 tile = st.container(height=200, border=True)
                 subtile = tile.container(height=110, border=False)
                 subtile.subheader(title)
-
 
                 if tile.button("학습하기", key=f"btn_{i}", use_container_width=True):
                     st.session_state['page'] = f"btn_{i}"
@@ -68,11 +84,3 @@ if st.session_state['page'] and st.session_state['page'].startswith('btn_'):
         
         if st.button("돌아가기"):
             st.session_state['page'] = selected
-
-# pandas 기초 페이지
-if st.session_state['page'] == "pandas 기초":
-    st.title("pandas 기초")
-
-# Matplotlib 기초 페이지
-if st.session_state['page'] == "Matplotlib 기초":
-    st.title("Matplotlib 기초")
