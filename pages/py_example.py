786400159e2065564fb6e17390e1732ed5a8f265
홍서정 <94052799+hsj517@users.noreply.github.com>
2024-07-24 00:40:33 +0900

    refactor

--- pages/py_example.py
+++ pages/py_example.py
@@ -3,12 +3,6 @@ from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
 
-if 'topic' not in st.session_state:
-    st.session_state['topic'] = None
-
-if 'chapter' not in st.session_state:
-    st.session_state['chapter'] = None
-
 #topic - chapter - section
 topics = {
     "파이썬 기초": {
@@ -28,6 +22,8 @@ topics = {
         "대단원 02": ["소단원01", "소단원02"]
     }
 }
+if 'page' not in st.session_state:
+    st.session_state['page'] = list(topics.keys())[0]
 
 # 사이드바 옵션
 with st.sidebar:
@@ -44,50 +40,16 @@ with st.sidebar:
 
 #사이드바 -> 대단원
 if selected :
-    st.session_state['topic'] = selected
-    st.session_state['chapter'] = None
+    st.session_state['page'] = selected
 
-placeholder = st.empty()
-if st.session_state['topic']:
-    topic = st.session_state['topic']
-    with placeholder.container():
-        st.title(topic)
+def show_topic():
+    st.write(st.session_state['page'])
 
-        info_txt = {
-            "파이썬 기초" : "파이썬 기초 문법을 제공합니다.",
-            "Pandas 기초" : "Pandas 기초 문법을 제공합니다.",
-            "Matplotlib 기초" : "Matplotlib 기초 문법을 제공합니다.",
-        }
-        st.info(info_txt[topic])
-        
-        # 대단원
-        chapters = topics[topic]
-        table = [st.columns(3)] * ((len(chapters) + 2) // 3)
-        
-        for i, title in enumerate(chapters):
-            with table[i // 3][i % 3]:
-                tile = st.container(height=200, border=True)
-                subtile = tile.container(height=110, border=False)
-                subtile.subheader(title)
+def show_chapter():
+    st.write("챕터")
 
-                if tile.button("학습하기", key=f"btn_{i}", use_container_width=True):
-                    st.session_state['chapter'] = title
+def show_section():
+    st.write("섹션")
 
-# 대단원 페이지
-if st.session_state['topic'] and st.session_state['chapter']:
-    chapter = st.session_state['chapter']
-    with placeholder.container():
-        st.header(st.session_state['chapter'])
-        # 소단원
-        section = st.selectbox("Choose a topic:", 
-                               topics[topic][chapter], label_visibility="hidden")
-        
-        st.subheader("예시코드01")
-        with st.echo():
-            import pandas as pd
-            df = pd.DataFrame()
-        st.divider()
-        
-        if st.button("돌아가기"):
-            st.session_state['topic'] = selected
-            st.session_state['chapter'] = None
+if st.session_state['page'] :
+    show_topic()
