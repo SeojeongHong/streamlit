8c5a7a1c914c755c079f3e7b17cd91e1e74b19e9
SeojeongHong <94052799+SeojeongHong@users.noreply.github.com>
2024-08-16 00:39:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import io
 from matplotlib import font_manager as fm, rc
 fpath = os.path.join(os.getcwd(), "customfont/NanumGothic-Regular.ttf")
 prop = fm.FontProperties(fname=fpath).get_name()
-rc('prop', family = prop)
+rc('font', family = prop)
 import numpy as np
 import seaborn as sns
 import pandas as pd
