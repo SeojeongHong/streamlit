3e2943acd2bb17b463ffe9f728ee7256d1303252
SeojeongHong <94052799+SeojeongHong@users.noreply.github.com>
2024-08-19 10:06:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6156,19 +6156,18 @@ plt.show()'''
     
         st.write('이러한 시각화 자료를 통해 설득력을 더욱 높일 수 있습니다.')
 
-
-
     elif path == ("실습 프로젝트", "날씨별 공공자전거 수요 분석"):
         st.header(f"{idx.getHeadIdx()}날씨별 공공자전거 수요 분석")
         st.write('''
-                자전거 대여소는 계절과 날씨에 따라 대여 건수의 변동이 심해, 운영 비용에 큰 영향을 미치고 있습니다. 따라서 날씨예보정보를 활용해 대여건수를 사전에 예측하고, 
+                자전거 대여소는 계절과 날씨에 따라 대여 건수의 변동이 심해, 운영 비용에 큰 영향을 미치고 있습니다. 따라서 날씨 예보 정보를 활용해 대여 건수를 사전에 예측하고, 
                  운영 비용을 조정하기 위한 데이터 분석 및 시각화 실습을 진행합니다.
                  ''')
         st.divider()
 
-        st.subheader(f"{idx.getSubIdx()}데이터 불러오기")
-        st.write('- 실습을 위해 **아래의 버튼**을 클릭하여 데이터를 다운로드 해주세요')
-        st.write('해당 파일을 압축 해제해 **실습03** 폴더를 :blue-background[data/실습03/]경로로 이동해주세요.')
+
+        st.subheader(f"{idx.getSubIdx()}데이터 준비")
+        st.write('- 실습을 위해 **아래의 버튼**을 클릭하여 데이터를 다운로드해 주세요')
+        st.write('해당 파일을 압축 해제해 **실습03** 폴더를 :blue-background[data/실습03/]경로로 이동해 주세요.')
         with open('data/실습03.zip', "rb") as template_file:
             template_zip = template_file.read()
 
@@ -6177,18 +6176,37 @@ plt.show()'''
                             data=template_zip,
                            file_name = "실습03.zip"
         )
+        st.divider()
+
+
+        st.subheader(f"{idx.getSubIdx()}패키지 설치 및 호출")
+        st.write('''
+                CMD 창을 열고 아래의 패키지들을 설치해 줍니다. 
+                 ''')
         st.code('''
-                # 필요한 패키지 설치
+                $ pip install numpy
+                ''', language="text")
+        st.code('''
+                $ pip install pandas
+                ''', language="text")
+        st.code('''
+                $ pip install seaborn
+                ''', language="text")
+        st.code('''
+                $ pip install matplotlib
+                ''', language="text")
+        
+        st.write("다시 작업 파일(.ipynb)로 돌아와서, 설치한 패키지들을 호출해 줍니다.")
+        st.code('''
                 import numpy as np
                 import pandas as pd
                 import seaborn as sns
                 import matplotlib.pyplot as plt
+                ''')
+        st.divider()
 
-                #한글 표시
-                plt.rcParams['font.family'] = 'NanumGothic'
-                plt.rc('font', family='NanumGothic')
-                ''', line_numbers=True)
 
+        st.subheader(f"{idx.getSubIdx()}데이터 불러오기")
         st.write("실습에 필요한 데이터를 불러오겠습니다.")
         st.code('''
                 # 기상관측자료 데이터
@@ -6207,7 +6225,6 @@ plt.show()'''
                 #파일 병합
                 bike_info = pd.concat([pd.read_csv(file, encoding='cp949') for file in files], ignore_index=True)
                 ''', line_numbers=True)
-        #------------------------------------------------------------
         
         import numpy as np
         import pandas as pd
@@ -6229,7 +6246,6 @@ plt.show()'''
 
         #파일 병합
         bike_info = pd.concat([pd.read_csv(file, encoding='cp949') for file in files], ignore_index=True)
-        #------------------------------------------------------------
         st.write("**weather_info**")
         st.code('''weather_info.sample(5)''', line_numbers=True)
         st.write(weather_info.sample(5))
@@ -6239,9 +6255,10 @@ plt.show()'''
         st.write(bike_info.sample(5))
         st.divider()
         
+
         st.header(f"{idx.getHeadIdx()}공공자전거 데이터 전처리")
         st.subheader(f"{idx.getSubIdx()}집계 데이터 생성")
-        st.write('''날씨 정보와의 결합에 필요한 데이터(**이용건수**)를 생성하기 위해 **대여일자**, **대여시간**으로 집계해줍니다.''')
+        st.write('''날씨 정보와의 결합에 필요한 데이터(**이용건수**)를 생성하기 위해 **대여일자**, **대여시간**으로 집계해 줍니다.''')
         st.code('''
                 #공공자전거 집계 데이터 생성
                 bike_df2 = bike_info.groupby(['대여일자', '대여시간'])['이용건수'].sum()
@@ -6253,6 +6270,7 @@ plt.show()'''
         bike_df2 = bike_df2.reset_index() #인덱스 재 정렬 , 기존 인덱스를 열로
         st.write(bike_df2.sample(5))
         st.divider()
+
 
         st.subheader(f"{idx.getSubIdx()}파생변수 생성")
         st.write('''대여일자에서 **년도, 월, 일, 요일, 공휴일** 변수를 생성합니다.''')
@@ -6282,6 +6300,7 @@ plt.show()'''
         st.write(bike_df2.sample(5))
         st.divider()
 
+
         st.header(f"{idx.getHeadIdx()}날씨 데이터 전처리")
         st.subheader(f"{idx.getSubIdx()}날짜, 시간 컬럼 생성")
         st.write('''자전거 이용정보와의 결합을 위해 **일시** 칼럼에서 **날짜**와 **시간** 정보를 추출합니다.''')
@@ -6302,6 +6321,7 @@ plt.show()'''
         st.text(buffer.getvalue())
         st.divider()
 
+
         st.subheader(f"{idx.getSubIdx()}컬럼 선택")
         st.write("분석에 사용할 컬럼을 순서대로 가져와서 새 데이터 프레임을 생성합니다.")
         st.code('''
@@ -6319,6 +6339,7 @@ plt.show()'''
         st.write(weather_df.columns)
         st.divider()
         
+
         st.subheader(f"{idx.getSubIdx()}결측치 확인")
         st.code('''
                 #결측치 확인
@@ -6326,7 +6347,7 @@ plt.show()'''
                 ''',line_numbers=True)
         st.write(weather_df.isnull().sum())
         st.write('''
-                **강수량, 적설, 일조, 일사**와 같이 NaN값이 0인 경우는 0으로 fill 해줍니다. **전운량, 기온, 지면온도, 풍향, 풍속**은 같은 일자의 이전시간대의 데이터로 대체합니다.
+                **강수량, 적설, 일조, 일사**와 같이 NaN값이 0인 경우는 0으로 fill 해줍니다. **전운량, 기온, 지면온도, 풍향, 풍속**은 같은 일자의 이전 시간대의 데이터로 대체합니다.
                 ''')
         
         st.write('''
@@ -6359,11 +6380,7 @@ plt.show()'''
                 weather_df['전운량'] = weather_df['전운량'].ffill()
                 weather_df['지면온도'] = weather_df['지면온도'].ffill()
                 ''',line_numbers=True)
-        # NaN 값을 직전 데이터의 값으로 fill (ffill)
-        # 날짜 시간으로 정렬
         weather_df = weather_df.sort_values(['날짜','시간'])
-
-        # 전 값으로 
         weather_df['기온'] = weather_df['기온'].ffill()
         weather_df['풍속(m/s)']= weather_df['풍속(m/s)'].ffill()
         weather_df['풍향(16방위)'] = weather_df['풍향(16방위)'].ffill()
@@ -6378,6 +6395,7 @@ plt.show()'''
         st.write(weather_df.isnull().sum())
         st.divider()
 
+
         st.header(f"{idx.getHeadIdx()}데이터 결합")
         st.write("전처리된 공공자전거 데이터와 날씨 데이터를 결합해 날씨별 자전거 대여 데이터를 만들어보겠습니다.")
         st.code('''
@@ -6391,27 +6409,24 @@ plt.show()'''
                                     right_on = ['날짜', '시간']) #default = inner 
                 bike_mg.head()
                 ''',line_numbers=True)
-        #데이터 결합
         weather_df['날짜'] = pd.to_datetime(weather_df['날짜'])
-
-        #데이터 타입 맞추기 
         bike_mg = pd.merge (bike_df2, 
                             weather_df, 
                             left_on =['대여일자', '대여시간'], 
                             right_on = ['날짜', '시간']) #default = inner 
         st.write(bike_mg.head())
 
-        st.write("**대여일자, 날짜, 시간** 데이터가 중복되는 것을 확인할 수 있습니다. 중복되는 데이터를 제거해보겠습니다.")
+        st.write("**대여일자, 날짜, 시간** 데이터가 중복되는 것을 확인할 수 있습니다. 중복되는 데이터를 제거해 보겠습니다.")
         st.code('''
                 #중복데이터 제거
                 bike_mg = bike_mg.drop(['대여일자', '날짜', '시간'], axis = 1)
 
                 bike_mg.head()
                 ''',line_numbers=True)
-        #중복데이터 제거
         bike_mg = bike_mg.drop(['대여일자', '날짜', '시간'], axis = 1)
         st.write(bike_mg.head())
         st.divider()
+
 
         st.header(f"{idx.getHeadIdx()}데이터 시각화")
         st.write("원본 데이터프레임을 보존하기 위해 복사본을 생성한 후 시각화를 진행하겠습니다.")
@@ -6421,8 +6436,17 @@ plt.show()'''
                 ''',line_numbers=True)
         data = bike_mg.copy()
 
+        st.write("그래프를 그리기에 앞서, 한글 출력을 위한 폰트 설정을 해줍니다.")
+        st.code('''
+                #한글 표시
+                plt.rcParams['font.family'] = 'NanumGothic'
+                plt.rc('font', family='NanumGothic')
+                ''', line_numbers=True)
+        st.divider()
+
+
         st.subheader(f"{idx.getSubIdx()}데이터 요약 통계")
-        st.write("데이터의 요약 통계를 확인해 정상적인 값인지 확인해보겠습니다.")
+        st.write("데이터의 요약 통계를 확인해 정상적인 값인지 확인해 보겠습니다.")
         st.code('''
                 #데이터 요약 통계
                 desc_df = data.describe().T
@@ -6431,6 +6455,7 @@ plt.show()'''
         desc_df = data.describe().T
         st.write(desc_df)
         st.divider()
+
 
         st.subheader(f"{idx.getSubIdx()}이용건수 분포 시각화")
         st.code('''
@@ -6443,11 +6468,8 @@ plt.show()'''
         sns.histplot(data['이용건수'], ax=ax)
         ax.set_title("이용건수 분포", fontproperties=prop)
         ax.set_xlabel("이용건수", fontproperties=prop)
-        
         st.pyplot(fig)
         plt.close(fig)
-        
-
         st.code('''
                 sns.lineplot(x=data['일'], y=data['이용건수'])
 
@@ -6457,11 +6479,10 @@ plt.show()'''
         sns.lineplot(x=data['일'], y=data['이용건수'], ax=ax)
         ax.set_xlabel("일", fontproperties=prop)
         ax.set_ylabel("이용 건수", fontproperties=prop)
-        
         st.pyplot(fig)
         plt.close(fig)
-
         st.divider()
+
         
         st.subheader(f"{idx.getSubIdx()}피처의 분포 시각화")
         st.write("원하는 컬럼을 선택해 피처의 분포를 확인합니다.")
@@ -6495,6 +6516,7 @@ plt.show()'''
         st.pyplot(fig)
         plt.close(fig)
         st.divider()
+
 
         st.subheader(f"{idx.getSubIdx()}이용건수와 피처와의 관계")
         st.write("공공자전거 이용 건수와 피처와의 관계를 시각화합니다.")
@@ -6544,8 +6566,6 @@ plt.show()'''
 
         # 간격조정
         fig.subplots_adjust(hspace = 0.4)
-
-        
         st.pyplot(fig)
         plt.close(fig)
         st.write('''
@@ -6553,8 +6573,8 @@ plt.show()'''
                 - 기온이 높을수록 이용건수가 증가하는 경향을 보입니다.
                 - 강수량이 적을수록 이용건수가 높습니다.
                 ''')
-    
         st.divider()
+
 
         st.subheader(f"{idx.getSubIdx()}평일과 공휴일 이용건수 차이")
         st.code('''
@@ -6562,7 +6582,6 @@ plt.show()'''
             
                 plt.show()
                 ''',line_numbers=True)
-        
         fig, ax = plt.subplots()
         sns.pointplot(x='대여시간', y='이용건수',data = data, hue = '공휴일', ax=ax)
         ax.set_xlabel("대여시간", fontproperties=prop)
@@ -6574,9 +6593,10 @@ plt.show()'''
         st.pyplot(fig)
         plt.close(fig)
         st.write('''평일과 공휴일에는 완전히 다른 이용 현황을 보이는 것을 확인할 수 있습니다.
-                 평일의 경우 오전 8시, 오후 6시에 이용건수 피크를 보이는데, 출퇴근으로 인한 영향으로 추측해볼 수 있겠습니다.
+                 평일의 경우 오전 8시, 오후 6시에 이용건수 피크를 보이는데, 출퇴근으로 인한 영향으로 추측해 볼 수 있겠습니다.
                  ''')
         st.divider()
+
 
         st.subheader(f"{idx.getSubIdx()}요일에 따른 이용건수 차이")
         st.code('''
@@ -6595,8 +6615,8 @@ plt.show()'''
         st.pyplot(fig)
         plt.close(fig)
         st.write("토요일에 이용건수가 더 많고, 토요일 오후에 전반적으로 이용률이 높은 모습을 보입니다.")
-        
         st.divider()
+
 
         st.subheader(f"{idx.getSubIdx()}요일에 따른 이용건수 차이(box)")
         st.code('''
@@ -6616,11 +6636,11 @@ plt.show()'''
         st.pyplot(fig)
         plt.close(fig)
         st.write("공휴일은 상대적으로 변동성이 적고, 평일은 변동성이 큰 편입니다.")
-        
         st.divider()
         
+        
         st.header(f"{idx.getHeadIdx()}결론 도출")
-        st.subheader(f"{idx.getSubIdx()}시간대 및 공휴일여부에 따른 특성")
+        st.subheader(f"{idx.getSubIdx()}시간대 및 공휴일 여부에 따른 특성")
         st.write('''
                 - 공공자전거 이용이 가장 많은 시간대는 **평일 오후 6시**입니다.
                 - 두 번째로 이용이 많은 시간대는 **평일 오전 8시**입니다.
@@ -6635,6 +6655,7 @@ plt.show()'''
                 - 강수량이 증가할수록 이용건수가 **감소**하는 추세를 보입니다.
                 - 습도, 풍속, 일조량도 이용건수에 **영향**을 미치는 것으로 보입니다.
                 ''')
+        st.divider()
         st.divider()
         
     else :
