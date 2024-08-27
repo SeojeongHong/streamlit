62fc4bebc1f5fc08c8c5ac9211aace719c772599
SeojeongHong <94052799+SeojeongHong@users.noreply.github.com>
2024-08-27 09:41:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import seaborn as sns
 from streamlit_float import *
 import sqlite3
 from streamlit.web.server.websocket_headers import _get_websocket_headers
-import request
 
 def get_ip():
     # headers = _get_websocket_headers()
